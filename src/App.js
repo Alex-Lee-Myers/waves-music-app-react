139a4066d9ac6b1b4044f94c1b9d129f170d70fd
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-01-16 14:21:48 -0500

    added copyright free music to data.js to avoid any takedown. had to manually create dataset from chillhop as API was unavailable.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Song from './components/Song.jsx';
 import Player from './components/Player.jsx';
 // importing Styles/scss file
 import './styles/app.scss';
+// import data from data.js
+import data from './data.js';
 
 function App() {
   return (
